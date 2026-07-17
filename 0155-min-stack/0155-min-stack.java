64e11c250538b6ce3616db576c6bc0737278b72d
Punith N S <punithns26@gmail.com>
2026-07-17 10:11:19 +0530

    Time: 39 ms (6.22%), Space: 99.8 MB (50.95%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,43 +1,36 @@
 class MinStack {
-    int[] stack;
-    int[] minstack;
-    int top;
-    int top1;
+
+    Stack<Integer> arr = new Stack<>();
+    Stack<Integer> arr1 = new Stack<>();
+
     public MinStack() {
-        stack=new int[100000];
-        minstack=new int[100000];
-        top=-1;
-        top1=-1;
+
     }
-    
+
     public void push(int value) {
-        if(top==stack.length-1)return;
-        if(top==-1){
-            minstack[++top1]=value;
-            stack[++top]=value;
-            return;
+
+        arr.push(value);
+
+        if (arr1.isEmpty()) {
+            arr1.push(value);
+        } else {
+            arr1.push(Math.min(value, arr1.peek()));
         }
-        stack[++top] = value;
-
-        int currentMin = minstack[top1];
-        minstack[++top1] = Math.min(currentMin, value);
 
     }
-    
+
     public void pop() {
-        if(top==-1)return;
-        top--;
-        top1--;
+        int x = arr.pop();
+        int y = arr1.pop();
     }
-    
+
     public int top() {
-        if(top==-1)return -1;
-        return stack[top];
+        return arr.peek();
+
     }
-    
+
     public int getMin() {
-        if(top1==-1)return -1;
-        return minstack[top1];
+        return arr1.peek();
     }
 }
 

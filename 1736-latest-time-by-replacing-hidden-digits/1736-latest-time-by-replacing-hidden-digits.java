1a37369724f37aab9625f3b0f53921bbefa69e30
Punith N S <punithns26@gmail.com>
2026-08-06 16:24:21 +0530

    Time: 0 ms (100%), Space: 43.1 MB (20.66%) - LeetHub

---
+++ 1736-latest-time-by-replacing-hidden-digits/1736-latest-time-by-replacing-hidden-digits.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String maximumTime(String time) {
+
+        StringBuilder s = new StringBuilder(time);
+
+        if (s.charAt(0) == '?') {
+            if (s.charAt(1) == '?' || s.charAt(1) <= '3')
+                s.setCharAt(0, '2');
+            else
+                s.setCharAt(0, '1');
+        }
+        if (s.charAt(1) == '?') {
+            if (s.charAt(0) == '2')
+                s.setCharAt(1, '3');
+            else
+                s.setCharAt(1, '9');
+        }
+        if (s.charAt(3) == '?') {
+            s.setCharAt(3, '5');
+        }
+        if (s.charAt(4) == '?') {
+            s.setCharAt(4, '9');
+        
+    }
+            return s.toString();
+    }
+}
